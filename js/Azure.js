32882868d67a122d8a1cf80fbdbb2b5aebf29935
Dineshkumar510 <dk6089567@gmail.com>
2021-10-06 16:41:43 +0530

    My first commit

--- js/Azure.js
+++ js/Azure.js
@@ -5,10 +5,8 @@ window.onscroll = function() {scroll()};
         function scroll() {
           if (document.body.scrollTop > 780) {
             navbar.style.top = "0";
-            navbar.style.visibility = true;
           } else {
             navbar.style.top = "-160px";
-            navbar.style.visibility = false;
           }
         }
 
